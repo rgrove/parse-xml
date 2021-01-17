09771247f9c30ed4f8f9726107a775c6ac3693ad
Ryan Grove <ryan@wonko.com>
2021-01-16 20:43:33 -0800

    Add tests for XmlNode

--- tests/lib/XmlNode.test.js
+++ tests/lib/XmlNode.test.js
@@ -4,6 +4,22 @@
 const assert = require('assert');
 const parseXml = require('../../src');
 
-describe('`XmlNode`', () => {
+const { XmlNode } = parseXml;
 
+describe('`XmlNode`', () => {
+  describe('`document`', () => {
+    describe('when the node is not associated with a document', () => {
+      it('is `null`', () => {
+        let node = new XmlNode();
+        assert.strictEqual(node.document, null);
+      });
+    });
+  });
+
+  describe('`type`', () => {
+    it('is an empty string', () => {
+      let node = new XmlNode();
+      assert.strictEqual(node.type, '');
+    });
+  });
 });
