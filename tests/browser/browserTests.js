6a39276bfd01d079670b773f3f8de7ed23d0c251
Ryan Grove <ryan@wonko.com>
2021-01-16 20:43:12 -0800

    Add tests for XmlProcessingInstruction

--- tests/browser/browserTests.js
+++ tests/browser/browserTests.js
@@ -6,5 +6,6 @@ require('../lib/XmlComment.test.js');
 require('../lib/XmlDocument.test.js');
 require('../lib/XmlElement.test.js');
 require('../lib/XmlNode.test.js');
+require('../lib/XmlProcessingInstruction.test.js');
 require('../lib/XmlText.test.js');
 require('../conformance.test.js');
---
+++ tests/lib/XmlProcessingInstruction.test.js
@@ -0,0 +1,47 @@
+/* eslint-env mocha */
+'use strict';
+
+const assert = require('assert');
+const parseXml = require('../../src');
+
+const { XmlProcessingInstruction, XmlNode } = parseXml;
+
+describe('`XmlProcessingInstruction`', () => {
+  it('is emitted by the parser', () => {
+    let { root } = parseXml('<root><?xml-stylesheet type="text/xsl" href="style.xsl"?></root>');
+    assert(root.children[0] instanceof XmlProcessingInstruction);
+  });
+
+  it('can be serialized to JSON', () => {
+    let { root } = parseXml('<root><?xml-stylesheet type="text/xsl" href="style.xsl"?></root>');
+    assert.strictEqual(JSON.stringify(root.children[0]), `{"type":"pi","name":"xml-stylesheet","content":"type=\\"text/xsl\\" href=\\"style.xsl\\""}`);
+  });
+
+  describe('constructor', () => {
+    it('defaults the value of `content` to an empty string if not provided', () => {
+      let pi = new XmlProcessingInstruction('foo');
+      assert.strictEqual(pi.content, '');
+    });
+  });
+
+  describe('`document`', () => {
+    it('is the document', () => {
+      let doc = parseXml('<root><?foo?></root>');
+      assert.strictEqual(doc.root.children[0].document, doc);
+    });
+  });
+
+  describe('`parent`', () => {
+    it('is the parent element', () => {
+      let { root } = parseXml('<root><?foo?></root>');
+      assert.strictEqual(root.children[0].parent, root);
+    });
+  });
+
+  describe('`type`', () => {
+    it('is `XmlNode.TYPE_PROCESSING_INSTRUCTION`', () => {
+      let { root } = parseXml('<root><?foo?></root>');
+      assert.strictEqual(root.children[0].type, XmlNode.TYPE_PROCESSING_INSTRUCTION);
+    });
+  });
+});
