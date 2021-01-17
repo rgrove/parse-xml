b5e2d8d0f1bc6346b1d7ef30bed5c1bddcafb0f7
Ryan Grove <ryan@wonko.com>
2021-01-16 20:51:55 -0800

    More comprehensive XmlText tests

--- tests/lib/XmlText.test.js
+++ tests/lib/XmlText.test.js
@@ -4,20 +4,37 @@
 const assert = require('assert');
 const parseXml = require('../../src');
 
-const { XmlText } = parseXml;
+const { XmlNode, XmlText } = parseXml;
 
 describe('`XmlText`', () => {
-  let xml;
-
-  beforeEach(() => {
-    xml = `<root> foo &amp;  bar\r\nbaz </root>`;
+  it('is emitted by the parser', () => {
+    let { root } = parseXml('<root>hello there</root>');
+    assert(root.children[0] instanceof XmlText);
   });
 
-  it('is emitted', () => {
-    let { root } = parseXml(xml);
-    let [ textNode ] = root.children;
+  it('can be serialized to JSON', () => {
+    let { root } = parseXml('<root> foo &amp;  bar\r\nbaz </root>');
+    assert.strictEqual(JSON.stringify(root.children[0]), '{"type":"text","text":" foo &  bar\\nbaz "}');
+  });
 
-    assert(textNode instanceof XmlText);
-    assert.strictEqual(textNode.text, ' foo &  bar\nbaz ');
+  describe('constructor', () => {
+    it('defaults `text` to an empty string if not provided', () => {
+      let textNode = new XmlText();
+      assert.strictEqual(textNode.text, '');
+    });
+  });
+
+  describe('`text`', () => {
+    it('is the text content of the text node', () => {
+      let { root } = parseXml('<root> foo &amp;  bar\r\nbaz </root>');
+      assert.strictEqual(root.children[0].text, ' foo &  bar\nbaz ');
+    });
+  });
+
+  describe('`type`', () => {
+    it('is `XmlNode.TYPE_TEXT`', () => {
+      let { root } = parseXml('<root>hi</root>');
+      assert.strictEqual(root.children[0].type, XmlNode.TYPE_TEXT);
+    });
   });
 });
